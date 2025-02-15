898bd4542015e71ef99cf6ebecb6da286caeab72
Artem <stilon670@gmail.com>
2025-02-15 13:17:51 +0200

    add func getemployees

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,35 @@
 const listWorkers = document.querySelector('ul');
 const items = [...document.querySelectorAll('li')];
 
-const sortList = () =>
-  items.sort(
+function convertStrToNumber(num) {
+  return +num.replace(',', '');
+}
+
+const sortList = (elements, list) => {
+  elements.sort(
     (a, b) =>
       convertStrToNumber(b.dataset.salary.slice(1)) -
       convertStrToNumber(a.dataset.salary.slice(1)),
   );
 
-sortList(items);
+  list.innerHTML = '';
+  elements.forEach((item) => listWorkers.appendChild(item));
+};
 
-listWorkers.innerHTML = '';
-items.forEach((item) => listWorkers.appendChild(item));
+const getEmployees = (list) => {
+  const employees = [];
 
-function convertStrToNumber(num) {
-  return +num.replace(',', '');
-}
+  for (const item of list) {
+    employees.push({
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    });
+  }
+
+  return employees;
+};
+
+sortList(items, listWorkers);
+getEmployees(items);
