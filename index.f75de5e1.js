778b4c84c320f6ef7e2691d0a295cfcc79dd30a2
Artem <stilon670@gmail.com>
2025-02-15 12:52:12 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const listWorkers = document.querySelector("ul");
+const items = [
+    ...document.querySelectorAll("li")
+];
+const sortList = ()=>items.sort((a, b)=>convertStrToNumber(b.dataset.salary.slice(1)) - convertStrToNumber(a.dataset.salary.slice(1)));
+sortList(items);
+listWorkers.innerHTML = "";
+items.forEach((item)=>listWorkers.appendChild(item));
+function convertStrToNumber(num) {
+    return +num.replace(",", "");
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
