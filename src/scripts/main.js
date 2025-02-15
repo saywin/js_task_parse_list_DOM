fed16afee1af8c66be3be27107345e8ad5c51d43
Artem <stilon670@gmail.com>
2025-02-15 12:51:39 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const listWorkers = document.querySelector('ul');
+const items = [...document.querySelectorAll('li')];
+
+const sortList = () =>
+  items.sort(
+    (a, b) =>
+      convertStrToNumber(b.dataset.salary.slice(1)) -
+      convertStrToNumber(a.dataset.salary.slice(1)),
+  );
+
+sortList(items);
+
+listWorkers.innerHTML = '';
+items.forEach((item) => listWorkers.appendChild(item));
+
+function convertStrToNumber(num) {
+  return +num.replace(',', '');
+}
